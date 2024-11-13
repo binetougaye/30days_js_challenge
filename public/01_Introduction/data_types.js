537aa7ee16bc0adb2cb4a66fe85b2767be03e5a0
binetougaye <fabigaye3@gmail.com>
2024-11-13 15:01:49 +0000

    introduction

---
+++ public/01_Introduction/data_types.js
@@ -0,0 +1,29 @@
+// use the JavaScript typeof operator to check different data types
+const myName = "Fabi"
+const myNumber = 33
+console.log(typeof (myName))
+// Declare four variables without assigning values
+let nom;
+let monAge;
+let childs;
+let birds;
+// Declare four variables with assigned values
+const monNom = "Fabi";
+const maPassion = "lecture";
+const monAmour = "Darling";
+const maSérie = "Vampire Diaries"
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+const firstName = "Fatou Binetou"
+const lastName = "Gaye"
+const maritalStatus = "single";
+const country = "Senegal"
+const age = 25
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+const personalInfos = firstName + lastName + maritalStatus + country + age
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+const myAge = 25;
+const yourAge = 24;
+console.log("My age is " + myAge + "." + "Your age is " + yourAge)
+
+
---
+++ public/01_Introduction/variables.js
@@ -0,0 +1,12 @@
+// Write a single line comment which says, comments can make code readable
+//  comments can make code readable
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/* comments can make code readable, 
+ easy to reuse and informative*/
+//  declare variables and assign string, boolean, undefined and null data types
+const stringType = "Hello"
+const booleanType = true;
+const undefinedType = undefined;
+const nullType = null
