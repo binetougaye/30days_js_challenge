5da8577bc85075e275ad469f506c1817cb4438a6
binetougaye <fabigaye3@gmail.com>
2025-01-04 16:25:09 +0000

    feat: display countries list

--- public/22_Dom_Object/script.js
+++ public/22_Dom_Object/script.js
@@ -2,15 +2,35 @@ const container = document.querySelector(".container")
 
 
 // display odd and even numbers
-for (let index = 0; index < 102; index++) {
-    const divElement = document.createElement('div')
-    container.appendChild(divElement)
-    divElement.textContent = index
-    if (index % 2 === 0) {
-        divElement.style.backgroundColor = 'green'
+// for (let index = 0; index < 102; index++) {
+//     const divElement = document.createElement('div')
+//     container.appendChild(divElement)
+//     divElement.textContent = index
+//     if (index % 2 === 0) {
+//         divElement.style.backgroundColor = 'green'
 
-    } else {
-        divElement.style.backgroundColor = 'yellow'
+//     } else {
+//         divElement.style.backgroundColor = 'yellow'
+//     }
+
+// }
+// Display countries
+const api = 'https://restcountries.com/v3.1/all'
+async function getCountries() {
+    try {
+        const response = await fetch(api)
+        const data = await response.json()
+        const divElement = document.createElement('div')
+        container.appendChild(divElement)
+        divElement.textContent = data?.map((item, index) => item.name.common)
+
+    } catch (error) {
+        console.log(error);
+
     }
 
+
+
+
 }
+getCountries()
